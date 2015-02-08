0b3ad09093f69546517fb09487c85440c8f67afd
Zenedith <github@zenedith.mailgun.org>
2015-02-09 00:43:50 +0100

    Updated dependency and newrelic config

--- api.js
+++ api.js
@@ -1,7 +1,10 @@
 require('newrelic');
 var myRestifyApi = require('my-restify-api');
+var UnauthorizedError = myRestifyApi.error.UnauthorizedError;
 var vehicleHistoryController = require('./lib/controller/vehicleHistory');
 var logger = require('./lib/logger/logger').logger;
+var oauth = require('./lib/auth/oauth');
+
 
 var PATH = '/api/vehicle-history';
 
@@ -25,12 +28,36 @@ var errorHandlers = {
   }
 };
 
+var cacheHandler = function (req, res, next) {
+  res.cache('public', {maxAge: 60});
+  res.header('Vary', 'Accept-Language');
+//    res.header('Last-Modified', new Date());
+  return next();
+};
+
+var authHandler = function (req, res, next) {
+  try {
+    oauth
+      .authorize(req)
+      .scope('openid')
+      .client('vehiclehistory')
+      .user();
+  }
+  catch (e) {
+    return next(new UnauthorizedError('Unauthorized error: ' + e.message));
+  }
+
+  return next();
+};
+
 var routes = {
   'get': [
     {
       options: {
         path: PATH, version: '1.0.0'
       },
+      authMethod: authHandler,
+      cache: cacheHandler,
       controllerMethod: vehicleHistoryController.checkVehicleHistoryV1
     }
   ]
--- newrelic.js
+++ newrelic.js
@@ -10,17 +10,31 @@ exports.config = {
   /**
    * Array of application names.
    */
-  app_name : ['vehicle-history'],
+  app_name: ['vehicle-history'],
   /**
    * Your New Relic license key.
    */
-  license_key : config.newrelic.license_key,
-  logging : {
+  license_key: config.newrelic.license_key,
+  ssl: true,
+  agent_enabled: true,
+  logging: {
     /**
      * Level at which to log. 'trace' is most useful to New Relic when diagnosing
      * issues with the agent, 'info' and higher will impose the least overhead on
      * production applications.
      */
-    level : config.newrelic.logging
+    level: config.newrelic.logging
+    /**
+     * Where to put the log file -- by default just uses process.cwd +
+     * 'newrelic_agent.log'. A special case is a filepath of 'stdout',
+     * in which case all logging will go to stdout, or 'stderr', in which
+     * case all logging will go to stderr.
+     *
+     * @env NEW_RELIC_LOG
+     */
+//    filepath: 'stdout'
+  },
+  browser_monitoring: {
+    enable: false
   }
 };
